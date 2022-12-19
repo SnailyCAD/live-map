7d8ef51635615ad2320d6014571688c51cd35c5a
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-12-19 19:28:14 +0100

    chore(release): 0.0.5

--- integration/fxmanifest.lua
+++ integration/fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Dev-CasperTheGhost"
 description "SnailyCAD Live Map integration for FiveM."
-version "0.0.4"
+version "0.0.5"
 
 client_scripts {"client/client.js"}
 server_scripts {"server/server.js"}
