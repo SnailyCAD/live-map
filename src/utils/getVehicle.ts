ce33850d3ab34f7340a4659788b24eaa162e28c4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-12-23 11:49:59 +0100

    :tada: feat: add siren enabled field

--- src/utils/getVehicle.ts
+++ src/utils/getVehicle.ts
@@ -6,11 +6,24 @@ export function getVehicle(pedId: number) {
   }
 
   const licensePlate = GetVehicleNumberPlateText(vehicle);
+  const hasSirenEnabled = isSirenEnabled(vehicle);
   let vehicleName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)));
 
   if (vehicleName === "NULL") {
     vehicleName = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle));
   }
 
-  return { licensePlate, vehicle: vehicleName };
+  return { licensePlate, vehicle: vehicleName, hasSirenEnabled };
+}
+
+function isSirenEnabled(vehicle: number) {
+  const sirenEnabledValue =
+    IsVehicleSirenOn(vehicle) || IsVehicleSirenAudioOn(vehicle) || IsVehicleSirenSoundOn(vehicle);
+
+  // it can also return a number (1 or 0)
+  if (typeof sirenEnabledValue === "number") {
+    return sirenEnabledValue === 1;
+  }
+
+  return sirenEnabledValue;
 }
